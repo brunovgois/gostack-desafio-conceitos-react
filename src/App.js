d0754bc151e7686bf92387b02ee1a0524de7d58d
bruno-gois <brunovgois@gmail.com>
2020-07-21 23:31:05 -0300

    add & remove projects

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import api from "./services/api"
 
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setProjects(response.data);
+    });
+  }, [] );
+
   async function handleAddRepository() {
-    // TODO
+    await api.post('repositories', {
+      title: 'Novo projeto',
+    })
+    .then(response => {
+      setProjects([...projects, response.data] )
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setProjects(projects.filter((projects) => projects.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          {projects.map(project => {
+            return (
+              <li key={project.id}>{project.title}
+                <button onClick={() => handleRemoveRepository(project.id)}>
+                  Remover
+                </button>
+              </li>
+          )
+        })}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
